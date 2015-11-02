022d510938b214934a5fb7adb180627fb74a1e17
Steve Aitken <steve.aitken@gmail.com>
2015-11-02 16:57:28 +0000

    update
    updated to calculate values that will be wrong but can be calibrated.

--- ArduniorowComputer/ArduniorowComputer.ino
+++ ArduniorowComputer/ArduniorowComputer.ino
@@ -17,6 +17,8 @@ unsigned long instantaneousrpm;
 unsigned long nextinstantaneousrpm;
 unsigned long startTime = 0;
 unsigned long rotations = 0;
+unsigned long laststrokerotations = 0;
+unsigned long laststroketime = 0;
 
 //unsigned long angularmomentum_gm2 = 100;//0.1001 kgm2 == 100.1g/m2 moment of intertia
 float I = 100.1/*moment of  interia*/;
@@ -24,11 +26,7 @@ float k = 170;/*drag factor*/
 float c = 2.8; //The figure used for c is somewhat arbitrary - selected to indicate a 'realistic' boat speed for a given output power.
          //Concept used to quote a figure c=2.8, which, for a 2:00 per 500m split (equivalent to u=500/120=4.17m/s) gives 203 Watts. 
 bool Accelerating;
-float theta1;//previous angular velocity
-float t1; //previous time
-float theta2; //most recent angular velocity
-float t2; //most recent time measurement.
-float w1; //previous angular acceleration
+
 
 void setup() 
 {
@@ -58,35 +56,45 @@ void loop()
                           //Serial.print("TimeTaken(ms):");
                           //Serial.println(timetakenms);
                           nextinstantaneousrpm = 60000/timetakenms;
-                          lcd.setCursor(0, 1);
-                          lcd.setCursor(0,1);
-                          lcd.print("                ");
-                          lcd.setCursor(0,1);
-                          lcd.print("RPM:");
-                          lcd.print(instantaneousrpm);
-                          lcd.print("distance:");
-                          lcd.print(rotations);
-                          //Serial.print("RPM:");
-                          //Serial.println(instantaneousrpm);
-                          lcd.setCursor(0,0);
-                          lcd.print("LOW ");
-                          resetT();
                           if(nextinstantaneousrpm >= instantaneousrpm)
                           {
                               lcd.print("Acc");        
                               if(!Accelerating)
                               {
                                 //beginning drive.
+
                               }
+
                               Accelerating = true;    
                           }
                           else
                           {
+
                               lcd.print("Dec");
                               if(Accelerating)
                               {
                                 //finished drive
+                                float diffrotations = laststrokerotations - rotations;
+                                long difft = laststroketime - millis();
+                                laststrokerotations = rotations;
+                                laststroketime = millis();
+                                float split =  (difft/1000)/ diffrotations ;
+                                lcd.setCursor(0, 1);
+                                lcd.setCursor(0,1);
+                                lcd.print("                ");
+                                lcd.setCursor(0,1);
+                                lcd.print("Split:");
+                                lcd.print(split);
                                 
+//                                lcd.print("RPM:");
+//                                lcd.print(instantaneousrpm);
+//                                lcd.print("distance:");
+//                                lcd.print(rotations);
+                                //Serial.print("RPM:");
+                                //Serial.println(instantaneousrpm);
+                                lcd.setCursor(0,0);
+                                lcd.print("D:");
+                                lcd.print(rotations);
                               }
                           }
                           //watch out for integer math problems here
@@ -112,32 +120,32 @@ void resetT()
   t2 = millis();
 }
 
-//return the power supplied in Watts - given angular velocity change, and time change.
-float PowerSupplied()
-{
-  float result = I * dw()/(dt()/(float)1000) * dtheta() + k *  dw()*dw() * dtheta();
-  //reset previous angular velocity;
-  w1 = dtheta()/dt();
-  return result;
-}
-
-//change in angular velocity
-float dw()
-{
-  return dtheta()/dt() - w1;
-}
-
-//change in angular velocity rad/s
-float dtheta()
-{
-  return 2 * 3.1415926535;
-}
-
-//time difference in milliseconds
-float dt()
-{
-  return t2-t1;
-}
+////return the power supplied in Watts - given angular velocity change, and time change.
+//float PowerSupplied()
+//{
+//  float result = I * dw()/(dt()/(float)1000) * dtheta() + k *  dw()*dw() * dtheta();
+//  //reset previous angular velocity;
+//  w1 = dtheta()/dt();
+//  return result;
+//}
+//
+////change in angular velocity
+//float dw()
+//{
+//  return dtheta()/dt() - w1;
+//}
+//
+////change in angular velocity rad/s
+//float dtheta()
+//{
+//  return 2 * 3.1415926535;
+//}
+//
+////time difference in milliseconds
+//float dt()
+//{
+//  return t2-t1;
+//}
 
 //k can be displayed on the Concept monitor (units 10-6 N m s2)
 //k = - I ( dω / dt ) (1 / ω2 ) = I d(1/ω) / dt
