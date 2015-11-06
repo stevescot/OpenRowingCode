c9aa5345b8fd3352b45ee90885c6787ab624d5d6
Steve Aitken <steve.aitken@gmail.com>
2015-11-06 16:48:57 +0000

    Add website for linking rowers together.

---
+++ RowingSite/Rowing/RowingSite/Upload.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RowingSite
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var mac = Request.QueryString("m");//mac address of this client
+            var t = Request.QueryString("t"); //time to this sample
+            var d = Request.QueryString("d");//distance (m)
+            var s = Request.QueryString("s");//split (seconds).
+        }
+    }
+}
