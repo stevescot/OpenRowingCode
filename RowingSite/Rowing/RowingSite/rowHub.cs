2d70f9a815889b47cad79b40558c268794c9d4ee
Steve Aitken <steve.aitken@gmail.com>
2015-11-09 17:38:44 +0000

    Add hub code

--- RowingSite/Rowing/RowingSite/rowHub.cs
+++ RowingSite/Rowing/RowingSite/rowHub.cs
@@ -8,9 +8,91 @@ namespace RowingSite
 {
     public class rowHub : Hub
     {
-        public void Hello()
+        /// <summary>
+        //
+        /// </summary>
+        public static Dictionary<string,List<string>> Users = new Dictionary<string, List<string>>();
+
+        public void updateStats(RowingSample rowsample)
         {
-            Clients.All.hello();
+            //Clients.All.;
+        }
+
+        /// <summary>
+        /// The OnConnected event.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            string clientId = GetClientId();
+
+            if (!Users.ContainsKey(clientId))
+            {
+                Users.Add(clientId, new List<string>());
+            }
+
+            // Send the current count of users
+            return base.OnConnected();
+        }
+
+        /// <summary>
+        /// The OnReconnected event.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            string clientId = GetClientId();
+            if (!Users.ContainsKey(clientId))
+            {
+                Users.Add(clientId, new List<string>());
+            }
+
+            // Send the current count of users
+            return base.OnReconnected();
+        }
+
+        /// <summary>
+        /// The OnDisconnected event.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            string clientId = GetClientId();
+
+            if (!Users.ContainsKey(clientId))
+            {
+                Users.Remove(clientId);
+            }
+
+            // Send the current count of users
+            return base.OnDisconnected(stopCalled);
+        }
+
+        /// <summary>
+        /// Get's the currently connected Id of the client.
+        /// This is unique for each client and is used to identify
+        /// a connection.
+        /// </summary>
+        /// <returns>The client Id.</returns>
+        private string GetClientId()
+        {
+            string clientId = "";
+            if (Context.QueryString["clientId"] != null)
+            {
+                // clientId passed from application 
+                clientId = this.Context.QueryString["clientId"];
+            }
+            if (string.IsNullOrEmpty(clientId.Trim()))
+            {
+                clientId = Context.ConnectionId;
+            }
+            return clientId;
         }
     }
 }
