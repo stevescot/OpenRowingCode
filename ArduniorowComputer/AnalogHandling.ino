cbd3a85779c51982eae67f4c894707d19e4efcd2
Steve Aitken <steve.aitken@gmail.com>
2015-12-14 16:17:01 +0000

    more case

--- ArduniorowComputer/AnalogHandling.ino
+++ ArduniorowComputer/AnalogHandling.ino
@@ -3,7 +3,7 @@
 static int AnalogMinValue = 4;              // minimum value of pulse to recognise the wheel as spinning
 static byte AnalogCountMin = 8;             // minimum number of values above the min value before we start monitoring
 byte AnalogCount = 0;                        // indicator to show how high the analog limit has been (count up /count down)
-byte lastanalogSwitchValue = 0;              // the last value read from the C2
+byte lastAnalogSwitchValue = 0;              // the last value read from the C2
 bool AnalogDropping = false;                // indicates if teh analog value is now dropping from a peak (wait until it gets to zero).
 
 void doAnalogRead()
@@ -12,7 +12,7 @@ void doAnalogRead()
     val = HIGH;
     if(!AnalogDropping)
     {
-      if(analog < lastanalogSwitchValue && (uTime- lastStateChangeus) >5000)
+      if(analog < lastAnalogSwitchValue && (uTime- lastStateChangeus) >5000)
       {//we are starting to drop - mark the value as low, and analog as dropping.
         //use this to see if the analog limit has tended to be above 6
         if(analog >= AnalogMinValue)
@@ -33,5 +33,5 @@ void doAnalogRead()
       }
     }
     if(analog== 0) AnalogDropping = false;//we have reached 0 - reset analog dropping so we can monitor for it once analog starts to drop.
-    lastanalogSwitchValue = analog;
+    lastAnalogSwitchValue = analog;
 }
